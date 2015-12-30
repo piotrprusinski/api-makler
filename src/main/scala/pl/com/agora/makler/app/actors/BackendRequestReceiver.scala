a6681efc1876402f5110627728591b825d2bf19f
Artur Bernat <Artur.Bernat@agora.pl>
2015-12-30 13:20:58 +0100

    add BackendRequestReceiver

--- src/main/scala/pl/com/agora/makler/app/actors/BackendRequestReceiver.scala
+++ src/main/scala/pl/com/agora/makler/app/actors/BackendRequestReceiver.scala
@@ -1,22 +1,20 @@
 package pl.com.agora.makler.app.actors
 
+
 import akka.actor.Actor
 import akka.actor.ActorLogging
 import akka.actor.ActorRef
 import akka.actor.Props
 import pl.com.agora.makler.app.messages.HandleRequest
 
-class BackendRequestSender extends Actor with ActorLogging {
+class BackendRequestReceiver extends Actor with ActorLogging {
 
   def receive  = {
     
     case msg : HandleRequest => {
       log.debug("Handling LearnApiKey message : {}", msg)
-      getNewInstanceBackendRequestReciver() ! msg
     }
   }
   
-   def getNewInstanceBackendRequestReciver ():ActorRef = {
-    context.actorOf(Props[BackendRequestReceiver])
-  }
+
 }
