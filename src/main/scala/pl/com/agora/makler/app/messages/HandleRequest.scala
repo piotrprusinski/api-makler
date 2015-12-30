2d75b2693de46e2b30a8bb283d84546ea6f11fd9
Artur Bernat <Artur.Bernat@agora.pl>
2015-12-30 11:50:05 +0100

    add toString in HandleRequest

--- src/main/scala/pl/com/agora/makler/app/messages/HandleRequest.scala
+++ src/main/scala/pl/com/agora/makler/app/messages/HandleRequest.scala
@@ -4,4 +4,8 @@ import pl.com.agora.makler.app.actors.ApiKey
 
 class HandleRequest(val apiKey:ApiKey) {
   
+    override def toString() : String = {
+    "HandleRequest with apiKey: " + apiKey.apiKey
+  }
+  
 }
