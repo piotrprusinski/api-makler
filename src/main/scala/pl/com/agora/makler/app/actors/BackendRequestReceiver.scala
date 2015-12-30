3690d868e34953daa5460d8a25495b400587a73c
Artur Bernat <Artur.Bernat@agora.pl>
2015-12-30 13:46:18 +0100

    add backend response timeout

--- src/main/scala/pl/com/agora/makler/app/actors/BackendRequestReceiver.scala
+++ src/main/scala/pl/com/agora/makler/app/actors/BackendRequestReceiver.scala
@@ -7,9 +7,11 @@ import akka.actor.ActorRef
 import akka.actor.Props
 import pl.com.agora.makler.app.messages.HandleRequest
 import pl.com.agora.makler.app.messages.BackendResponse
+import scala.concurrent.duration._
 
 class BackendRequestReceiver extends Actor with ActorLogging {
-
+context.setReceiveTimeout(scala.concurrent.duration.Duration(1,SECONDS))
+  
   def receive  = {
     case msg : BackendResponse => {
       log.debug("Handling message : {}", msg.msg)
